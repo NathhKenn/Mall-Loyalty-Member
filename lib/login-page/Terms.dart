a8fa7cc365f31f1e6ccfa836277de57d4e814420
Nathanael Kenn <nathanaelkenneth.nk@gmail.com>
2023-10-05 21:09:53 +0700

    tinggal nambahin scrollable text

--- lib/login-page/Terms.dart
+++ lib/login-page/Terms.dart
@@ -1,128 +1,158 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'dart:ui';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:myapp/utils.dart';
 import 'package:myapp/login-page/register.dart';
+
 
 class TermsScene extends StatelessWidget {
   const TermsScene({super.key});
-
   @override
   Widget build(BuildContext context) {
     double baseWidth = 390;
     double fem = MediaQuery.of(context).size.width / baseWidth;
     double ffem = fem * 0.97;
-    return Scaffold(
-      body: SizedBox(
+    return Container(
+      width: double.infinity,
+      child: Container(
+        // termsandagreetmentXMb (53:3)
         width: double.infinity,
-        child: 
-          Container(
-    width: 390,
-    height: 844,
-    clipBehavior: Clip.antiAlias,
-    decoration: const BoxDecoration(color: Colors.white),
-    child: Stack(
-        children: [
-            Positioned(
-                left: -311,
-                top: 0,
-                child: Container(
-                    width: 851,
-                    height: 844,
-                    decoration: const BoxDecoration(
-                        image: DecorationImage(
-                            image: NetworkImage("https://via.placeholder.com/851x844"),
-                            fit: BoxFit.fill,
-                        ),
-                    ),
-                ),
+        height: 844*fem,
+        decoration: BoxDecoration (
+          color: Color(0xffffffff),
+          image: DecorationImage (
+            fit: BoxFit.cover,
+            image: AssetImage (
+              'assets/login-page/images/pakuwon-mall-1-bg-TMT.png',
             ),
-            const Positioned(
-                left: 18,
-                top: 35,
+          ),
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              // rectangle22E17 (53:30)
+              left: 0*fem,
+              top: 26*fem,
+              child: ImageFiltered(
+                imageFilter: ImageFilter.blur (
+                  sigmaX: 2*fem,
+                  sigmaY: 2*fem,
+                ),
+                child: Align(
+                  child: SizedBox(
+                    width: 390*fem,
+                    height: 792*fem,
+                    child: Container(
+                      decoration: BoxDecoration (
+                        color: Color(0xffd9d9d9),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              // termsandconditionsiS5 (53:7)
+              left: 40.5*fem,
+              top: 35*fem,
+              child: Align(
                 child: SizedBox(
-                    width: 353,
-                    height: 38,
+                  width: 308*fem,
+                  height: 34*fem,
+                  child: Text(
+                    'Terms and Conditions',
+                    textAlign: TextAlign.center,
+                    style: SafeGoogleFont (
+                      'Inter',
+                      fontSize: 28*ffem,
+                      fontWeight: FontWeight.w900,
+                      height: 1.2125*ffem/fem,
+                      fontStyle: FontStyle.italic,
+                      color: Color(0xff000000),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              // line1C6M (53:34)
+              left: 0*fem,
+              top: 81.5*fem,
+              child: Align(
+                child: SizedBox(
+                  width: 390*fem,
+                  height: 3*fem,
+                  child: Container(
+                    decoration: BoxDecoration (
+                      color: Color(0xff000000),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              // cancelJ9P (53:36)
+              left: 150*fem,
+              top: 772*fem,
+              child: TextButton(
+                onPressed: (){
+                  Navigator.push(context,
+                  MaterialPageRoute(builder: ((context) => const RegisterScene())));
+                },
+                style: TextButton.styleFrom(
+                  padding:EdgeInsets.zero,
+                ),
+                child: Container(
+                  width: 90*fem,
+                  height: 30*fem,
+                  decoration: BoxDecoration (
+                    color: Color(0xff9bf4b4),
+                    borderRadius: BorderRadius.circular(6*fem),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0x3f000000),
+                        offset: Offset(0*fem, 4*fem),
+                        blurRadius: 2*fem,
+                      ),
+                    ],
+                  ),
+                  child: Center(
                     child: Text(
-                        'Terms and Conditions',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 28,
-                            fontStyle: FontStyle.italic,
-                            fontFamily: 'Inter',
-                            fontWeight: FontWeight.w900,
-                            height: 0,
-                        ),
+                      'BACK',
+                      textAlign: TextAlign.center,
+                      style: SafeGoogleFont (
+                        'Inter',
+                        fontSize: 15*ffem,
+                        fontWeight: FontWeight.w700,
+                        height: 1.2125*ffem/fem,
+                        color: Color(0xff000000),
+                      ),
                     ),
+                  ),
                 ),
+              ),
             ),
             Positioned(
-                left: -0,
-                top: 81.50,
-                child: Container(
-                    width: 390,
-                    decoration: const ShapeDecoration(
-                        shape: RoundedRectangleBorder(
-                            side: BorderSide(
-                                width: 3,
-                                strokeAlign: BorderSide.strokeAlignCenter,
-                            ),
-                        ),
-                    ),
-                ),
-            ),
-            Positioned(
-                left: 150,
-                top: 772,
+              // rectangle23iyo (53:35)
+              left: 9*fem,
+              top: 90*fem,
+              child: Align(
                 child: SizedBox(
-                    width: 90,
-                    height: 30,
-                    child: Stack(
-                        children: [
-                            Positioned(
-                                left: 0,
-                                top: 0,
-                                child: Container(
-                                    width: 90,
-                                    height: 30,
-                                    decoration: ShapeDecoration(
-                                        color: const Color(0xFF9BF4B4),
-                                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
-                                        shadows: const [
-                                            BoxShadow(
-                                                color: Color(0x3F000000),
-                                                blurRadius: 4,
-                                                offset: Offset(0, 4),
-                                                spreadRadius: 0,
-                                            )
-                                        ],
-                                    ),
-                                ),
-                            ),
-                            const Positioned(
-                                left: 0,
-                                top: 6,
-                                child: SizedBox(
-                                    width: 90,
-                                    child: Text(
-                                        'BACK',
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                            color: Colors.black,
-                                            fontSize: 15,
-                                            fontFamily: 'Inter',
-                                            fontWeight: FontWeight.w700,
-                                            height: 0,
-                                        ),
-                                    ),
-                                ),
-                            ),
-                        ],
+                  width: 371*fem,
+                  height: 665*fem,
+                  child: Container(
+                    decoration: BoxDecoration (
+                      borderRadius: BorderRadius.circular(20*fem),
+                      color: Color(0xffbebebe),
                     ),
+                  ),
                 ),
+              ),
             ),
-        ],
-    ),
-)
-      ) 
-      );
+          ],
+        ),
+      ),
+          );
   }
 }
